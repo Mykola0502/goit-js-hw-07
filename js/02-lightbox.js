94e2c6450c9c103639e5133e80a9e23708f30d58
Mykola Saliuk <haker0502@ukr.net>
2022-10-27 23:29:30 +0300

    додає код 2 завдання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryRefsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML(
+  'beforeend',
+  createGalleryRefsMarkup(galleryItems)
+);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});

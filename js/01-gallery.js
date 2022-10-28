4baaed2865f04bfd232ba7607c020413da791a53
Mykola Saliuk <haker0502@ukr.net>
2022-10-28 22:28:37 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
 const galleryContainer = document.querySelector('.gallery');
 
 galleryContainer.insertAdjacentHTML(
@@ -36,7 +37,11 @@ function onImageModalOpen(event) {
     return;
   }
   const imageUrl = event.target.dataset.source;
-  instance = basicLightbox.create(`<img width="1280" src="${imageUrl}">`);
+  instance = basicLightbox.create(`<img width="1280" src="${imageUrl}">`, {
+    onClose: instance => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    },
+  });
 
   instance.show();
 
@@ -51,9 +56,66 @@ function onImageModalOpen(event) {
   }
 
   function onCloseModal() {
-    window.removeEventListener('keydown', onEscKeyPress);
     instance.close();
   }
 }
 
 console.log(galleryItems);
+
+// const galleryContainer = document.querySelector('.gallery');
+
+// galleryContainer.insertAdjacentHTML(
+//   'beforeend',
+//   createGalleryRefsMarkup(galleryItems)
+// );
+
+// galleryContainer.addEventListener('click', onImageModalOpen);
+
+// function createGalleryRefsMarkup(galleryItems) {
+//   return galleryItems
+//     .map(({ original, preview, description }) => {
+//       return `<div class="gallery__item">
+//       <a class="gallery__link" href="${original}">
+//         <img
+//           class="gallery__image"
+//           src="${preview}"
+//           data-source="${original}"
+//           alt="${description}"
+//         />
+//       </a>
+//     </div>`;
+//     })
+//     .join('');
+// }
+
+// let instance;
+
+// function onImageModalOpen(event) {
+//   event.preventDefault();
+//   window.addEventListener('keydown', onEscKeyPress);
+
+//   if (!event.target.classList.contains('gallery__image')) {
+//     return;
+//   }
+//   const imageUrl = event.target.dataset.source;
+//   instance = basicLightbox.create(`<img width="1280" src="${imageUrl}">`);
+
+//   instance.show();
+
+//   function onEscKeyPress(event) {
+//     console.log(event);
+//     const ESC_KEY_CODE = 'Escape';
+//     const isEscKey = event.code === ESC_KEY_CODE;
+
+//     if (isEscKey) {
+//       onCloseModal();
+//     }
+//   }
+
+//   function onCloseModal() {
+//     window.removeEventListener('keydown', onEscKeyPress);
+//     instance.close();
+//   }
+// }
+
+// console.log(galleryItems);
